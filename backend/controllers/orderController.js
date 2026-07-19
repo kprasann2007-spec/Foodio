00b83a89d8cba546b7881fc58b4c24b262f651ae
pratiksha512 <pratikshakulkarni2006@gmail.com>
2026-07-19 11:38:17 +0530

    display orders and update status in admin panel

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -32,5 +32,26 @@ const userOrders = async (req, res) => {
         res.json({ success: false, message: "Error fetching orders" });
     }
 };
+//listing orders for admin panel
+const listOrders =async(req,res)=>{
+    try {
+        const orders=await orderModel.find({});
+        res.json({success:true,data:orders})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+    }
 
-export { placeOrder, userOrders };
+};
+// update order status (admin panel)
+const updateStatus = async (req, res) => {
+    try {
+        await orderModel.findByIdAndUpdate(req.body.orderId, { status: req.body.status });
+        res.json({ success: true, message: "Status Updated" });
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: "Error" });
+    }
+};
+
+export { placeOrder, userOrders ,listOrders,updateStatus};
